1b5c0245ef1e7fe28d53ebb48072ba25b4c50bf7
felipe.melo <felipe.melo@basis.com.br>
2017-11-27 13:34:49 -0200

    Subindo alterações p branch antiga

--- src/main/No.java
+++ src/main/No.java
@@ -1,9 +1,54 @@
 package main;
 
 public class No {
+	private Integer valor;
+	private No pai;
 
-    char data;
-    public No(char c) {
-        this.data=c;
-    }
+	public No(int valor) {
+		this.pai = null;
+		this.valor = valor;
+
+	}
+
+	public No(int valor, No pai) {
+		this.pai = pai;
+		this.valor = valor;
+
+	}
+	
+	public No getPai() {
+		return pai;
+	}
+
+	public void setPai(No pai) {
+		this.pai = pai;
+	}
+
+	public void setValor(int valor) {
+		this.valor = valor;
+	}
+
+	public Integer getValor() {
+		return valor;
+	}
+
+	public void setValor(Integer valor) {
+		this.valor = valor;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if ((obj instanceof No) && ((No) obj).valor.equals(this.valor)) {
+			return true;
+		}
+		return false;
+	}
+	
+	
+	@Override
+	public int hashCode() {
+		return valor;
+	}
+	
+
 }
